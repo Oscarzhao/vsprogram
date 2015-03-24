3e1d27ba0cbe341e89151fbdb1e0f8c07f81c7da
oscar <oscarzhaosl@gmail.com>
2015-03-24 22:49:07 +0800

    类继承，虚函数的所占的空间

--- EffectCpp/src/MemberFunctionPointer/main.cpp
+++ EffectCpp/src/MemberFunctionPointer/main.cpp
@@ -7,16 +7,45 @@
 class X
 {
 public:
+	int m_nInt;
+	//double m_dDouble;
+	
+	virtual ~X() {std::cout << "X destructor\n"; };
+	virtual void vmf() {std::cout << "virtual function of X \n"; }
     void mf(){ std::cout << "Class X::mf\n"; }
+	static void smf() {std::cout << "Class X::smf\n"; };
+
+};
+
+class Y: public X
+{
+public:
+	int m_nInt;
+
+	~Y(){ std::cout << "Y destructor\n"; }
+	void vmf() {std::cout << "virtual function of Y:\n"; }
+	static void ysmf(){std::cout << "Y: static function\n"; }
+	static void ysmf2() {std::cout << "Y::ysmf2 static function\n"; }
 };
 
 typedef void (X::*FunctionPtr)();
+
+void test()
+{
+	X x;
+	Y y;
+	std::cout << "sizeof(x) = " << sizeof(x) << ", sizeof(y) = " << sizeof(y) << std::endl;
+	std::cout << "sizeof(X) = " << sizeof(X) << ", sizeof(Y) = " << sizeof(Y) << std::endl;
+};
+
 int main()
 {
     // FunctionPtr pmf = X::mf; // error C3867
     FunctionPtr pmf = &X::mf;
-    X x;
+	X x;
     (x.*pmf)();
+
+	test();
     system("pause");
     return 0;
 }
